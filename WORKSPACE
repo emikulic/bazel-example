fe952716085f0289cebd2b60396be35c9022f0f3
Emil Mikulic <emikulic@gmail.com>
2020-02-19 20:23:19 +1100

    Bring in abseil and use flags.

--- WORKSPACE
+++ WORKSPACE
@@ -5,3 +5,9 @@ http_archive(
     strip_prefix = "rules_cc-master",
     urls = ["https://github.com/bazelbuild/rules_cc/archive/master.tar.gz"],
 )
+
+http_archive(
+    name = "com_google_absl",
+    strip_prefix = "abseil-cpp-master",
+    urls = ["https://github.com/abseil/abseil-cpp/archive/master.tar.gz"],
+)
--- src/hello.cc
+++ src/hello.cc
@@ -1,3 +1,11 @@
 #include <iostream>
 
-int main() { std::cout << "hello world\n"; }
+#include "absl/flags/flag.h"
+#include "absl/flags/parse.h"
+
+ABSL_FLAG(std::string, name, "world", "Who to say hello to");
+
+int main(int argc, char** argv) {
+  absl::ParseCommandLine(argc, argv);
+  std::cout << "Hello " << absl::GetFlag(FLAGS_name) << ".\n";
+}
