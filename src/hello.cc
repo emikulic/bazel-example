e7ea30d291e9f851afbe07492d85fe6937227c2f
Emil Mikulic <emikulic@gmail.com>
2020-02-19 20:25:26 +1100

    Use str_format.

--- src/hello.cc
+++ src/hello.cc
@@ -2,10 +2,11 @@
 
 #include "absl/flags/flag.h"
 #include "absl/flags/parse.h"
+#include "absl/strings/str_format.h"
 
 ABSL_FLAG(std::string, name, "world", "Who to say hello to");
 
 int main(int argc, char** argv) {
   absl::ParseCommandLine(argc, argv);
-  std::cout << "Hello " << absl::GetFlag(FLAGS_name) << ".\n";
+  std::cout << absl::StreamFormat("Hello %s.\n", absl::GetFlag(FLAGS_name));
 }
